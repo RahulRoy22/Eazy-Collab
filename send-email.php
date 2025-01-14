c219cc02fb01f212de4731d1e0b8344f954ecd57
Rahul Roy <66152751+RahulRoy22@users.noreply.github.com>
2025-01-14 18:50:28 +0530

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -18,15 +18,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; // Replace with your SMTP host
         $mail->SMTPAuth = true;
-        $mail->Username = 'isaacrao200121@gmail.com'; // Replace with your email
-        $mail->Password = 'apkf meob prvl zxvl'; // Replace with your email password
+        $mail->Username = ''; // Replace with your email
+        $mail->Password = ''; // Replace with your email password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         // Recipients
         $mail->setFrom($_POST['email'], $_POST['name']); // Use the user's email as the "From" email
         //$mail->addReplyTo($email, $name); // Use the user's email for replies as well
-        $mail->addAddress('isaacrao200121@gmail.com', 'isaac'); // Recipient's email
+        $mail->addAddress('', ''); // Recipient's email
         
         // Content
         $mail->isHTML(true);
